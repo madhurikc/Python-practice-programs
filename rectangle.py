34ab2934edb1245565847562025c8845646df391
madhurikc <kuruguntlamadhuri@gmail.com>
2025-03-06 06:52:56 -0600

    Area of a rectangle

---
+++ rectangle.py
@@ -0,0 +1,5 @@
+print("Find area of the Rectangle")
+length= int(input("enter the value: "))
+width= int(input("Enter the width: " ))
+rectangle = length*width
+print("Area of the rectangle is", rectangle)
