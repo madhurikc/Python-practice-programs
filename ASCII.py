33877be1bfb47f6e8d55e6f2272e6654ac8ef698
madhurikc <kuruguntlamadhuri@gmail.com>
2025-03-07 07:39:10 -0600

    Git commands

--- ASCII.py
+++ ASCII.py
@@ -1,3 +1,9 @@
+<<<<<<< HEAD
 char = input("Enter the character: ") # Input character from the user
 convert=ord(char) #helps to convert character to ASCII value 
 print(f"The ASCII value of '{char}' is {convert} ") # print the value
+=======
+char = input("Enter the character: ")
+convert=ord(char)
+print(f"The ASCII value of '{char}' is {convert} ")
+>>>>>>> b584ac07cf0774d240ee47ec4991f8aed85f6494
